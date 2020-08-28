875227f422760664c325157dfbea1fdfdebffa27
suxiaoxiao5 <1393231105@qq.com>
2020-08-28 14:40:28 +0800

    feat:初始化项目

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">
+        <h1>This is React App.</h1>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './common/style/frame.css'//引入全局样式
+
+ReactDOM.render(<App />, document.getElementById('root'))
